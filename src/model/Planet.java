42446a572f8304257e40d5827a53427d9858fcf6
LinaAcosta <linita.horan.05@gmail.com>
2019-08-24 12:02:09 -0500

    Prime method and definition of spaceship class

--- src/model/Planet.java
+++ src/model/Planet.java
@@ -1,5 +1,32 @@
-package model;
+import java.util.List;
 
 public class Planet {
-
+	private List<Spaceship> spaceships;
+	public Planet() {
+		spaceships = new ArrayList<>();
+	}
+	public List<Spaceship> getSpaceships() {
+		return spaceships;
+	}
+	public boolean isPrime(int n) {
+		boolean prime = true;
+		double number = Math.sqrt(n); 
+		int noDecimal = (int) Math.round(number);
+		if(number == noDecimal) {
+			prime = false;
+			System.out.println("no es primo");
+			return prime;
+		}
+		else {
+			if(n%2 != 0 && n%3 != 0 && n%5 != 0 && n%7 != 0 && n%11 != 0 && n%13 != 0) {
+				prime = false;
+				System.out.println("es primo");
+				return prime;
+			}else {
+				System.out.println("no es primo");
+				return prime;
+			}
+		}
+		
+	}
 }
--- src/model/Spaceship.java
+++ src/model/Spaceship.java
@@ -1,5 +1,21 @@
 package model;
 
 public class Spaceship {
-
+	private int coordinatex;
+	private int coordinatey;
+	private int number;
+	public Spaceship(int x, int y, int n) {
+		x = coordinatex;
+		y = coordinatey;
+		n = number;
+	}
+	public int getX() {
+		return coordinatex;
+	}
+	public int getY() {
+		return coordinatey;
+	}
+	public int getNumber() {
+		return number;
+	}
 }
