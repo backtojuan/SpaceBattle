f795f689cbf61bf25df1cad60f06323ecfffc334
Juan José Valencia <juanchouni@hotmail.com>
2019-08-25 12:20:00 -0500

    VenusTest class finished

--- test/model/VenusTest.java
+++ test/model/VenusTest.java
@@ -8,7 +8,7 @@
 	 * 
 	 * @author Lina Acosta Holguin
 	 * @author Lina Salinas Delgado
-	 * @author Maria Ordo�ez Ordo�ez
+	 * @author Maria Ordoñez Ordoñez
 	 * @author Juan Valencia Jaramillo
 	 * This class manage the necessary attributes and methods to represent the venus participant in the spatial battle
 	 */
@@ -69,6 +69,55 @@
 			
 			assertEquals(25, venus.getCoefficientMatrix().length, "the rows number is not the expected one");
 			assertEquals(200, venus.getCoefficientMatrix()[0].length, "the columns number is not the expected one");
+		}
+		
+	//-------------------------------------------------------------------------------------------------------------------------------------
+		
+		@Test 
+		public void testFillNonRepeatedMatrix() {
+			
+			setUpScenary2();
+			venus.setCoefficient(15, 25);
+			venus.setLast(10, 10);
+			venus.fillNonRepeatedMatrix();
+			int[][] coefficientMatrix = venus.getCoefficientMatrix();
+			for(int i = 0; i<coefficientMatrix.length;i++) {
+				for(int j = 0; j<coefficientMatrix[i].length; j++) {		
+					for(int z = 0; z<coefficientMatrix[i].length; z++) {
+						assertTrue((coefficientMatrix[i][j] != coefficientMatrix[i][z]),"the matrix is ​​filled incorrectly");
+					}
+				}
+			}
+			int[][] lastMatrix = venus.getLastMatrix();
+			for(int i = 0; i<lastMatrix.length;i++) {
+				for(int j = 0; j<lastMatrix[i].length; j++) {
+					for(int z = 0; z<lastMatrix[i].length; z++) {
+						assertTrue(lastMatrix[i][j] != lastMatrix[i][z],"the matrix is ​​filled incorrectly");
+					}
+				}
+			}
+		}
+		
+	//-------------------------------------------------------------------------------------------------------------------------------------
+		
+		@Test
+		public void testFillRepeatedMatrix() {
+			setUpScenary2();
+			venus.setCoefficient(15, 25);
+			venus.setLast(10, 10);
+			venus.fillRepeatedMatrix();
+			int[][] coefficientMatrix = venus.getCoefficientMatrix();
+			for(int i = 0; i<coefficientMatrix.length;i++) {
+				for(int j = 0; j<coefficientMatrix[i].length; j++) {
+					assertTrue(coefficientMatrix[i][j] != 0,"the matrix is ​​filled incorrectly");
+				}
+			}
+			int[][] lastMatrix = venus.getLastMatrix();
+			for(int i = 0; i<lastMatrix.length;i++) {
+				for(int j = 0; j<lastMatrix[i].length; j++) {
+					assertTrue(lastMatrix[i][j] != 0,"the matrix is ​​filled incorrectly");
+				}
+			}
 		}
 		
 //-----------------------------------------------------------------------------------------------------------------------------------------
